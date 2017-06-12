14d1c4f94e727c0c1746e746b8bc29a133094289
ravi tej <k.ravitej4u@gmail.com>
2017-06-12 13:56:58 -0400

    Added Crime Fragment

--- app/src/main/java/com/ravitej/fragmentswithviewpager/view/CrimeActivity.java
+++ app/src/main/java/com/ravitej/fragmentswithviewpager/view/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.ravitej.fragmentswithviewpager.view;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,17 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        /*Adding CrimeFragment to the CrimeActivity Screen*/
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_conatiner);
+
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_conatiner, fragment)
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/ravitej/fragmentswithviewpager/view/CrimeFragment.java
@@ -0,0 +1,91 @@
+package com.ravitej.fragmentswithviewpager.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import com.ravitej.fragmentswithviewpager.R;
+import com.ravitej.fragmentswithviewpager.model.Crime;
+
+import static android.widget.CompoundButton.*;
+
+/**
+ * Created by No One on 6/12/2017.
+ */
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+    private EditText mTitleText;
+    private Button mDateButton;
+    private CheckBox mIsSolved;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.crime_fragment, container, false);
+        /*Getting EditText View instance and adding listner to the EditText*/
+        mTitleText = (EditText)view.findViewById(R.id.crime_title);
+        setTitleListner();
+
+        /*Setting Date to the Button and Disabling it*/
+        mDateButton = (Button)view.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getmDate().toString());
+        mDateButton.setEnabled(false);
+
+        /*Listneing for check box changes*/
+        mIsSolved = (CheckBox)view.findViewById(R.id.crime_issolved);
+        setCheckBoxListner();
+
+        return view;
+    }
+
+    /*
+    * Adding Listner to the Check Box
+    * */
+    private void setCheckBoxListner() {
+       mIsSolved.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setmSolved(isChecked);
+            }
+        });
+    }
+
+
+    /*
+    *Adding Listner to the EditText Button for CRIME_TITLE
+    */
+    public void setTitleListner(){
+        mTitleText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setmTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
